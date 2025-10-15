ed8e97f1a0c39afe8af167d0be2e8f088beea3e8
HarishNarava <naravaharish941@gmail.com>
2025-10-15 08:05:05 -0400

    Project structure ready, venv set.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harish Narava",
+    author_email="naravah@gmail.com",
+    packages=find_packages()
+)
